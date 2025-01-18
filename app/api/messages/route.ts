9b3cb8a4e2538e845d8083ec662116c021410e54
JosephMaltese <josephmaltese34@gmail.com>
2025-01-18 00:31:47 -0500

    initialized prisma

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
+import prisma from '../../../../lib/prisma';
 
 export async function GET() {
     try {
@@ -28,3 +28,7 @@ export async function GET() {
     }
 }
 
+
+export async function POST(request : any) {
+    
+}
