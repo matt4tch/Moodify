5615296814bc194481abf5c84e7f9f0077775049
Matthew Tchouikine <matthew4.tch@gmail.com>
2025-01-17 23:46:59 -0500

    created messages endpoint

---
+++ app/api/messages/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '@/lib/prisma';
+
+export async function GET() {
+    try {
+        const currentDate = new Date();
+        currentDate.setHours(0, 0, 0, 0);
+
+        const messages = await prisma.message.findMany({
+            where: {
+                time: {
+                    gte: currentDate,
+                    lt: new Date(currentDate.getTime() + 24 * 60 * 60 * 1000),
+                },
+            },
+            include: {
+                user: true,
+            },
+            orderBy: {
+                date_created: 'desc',
+            },
+        });
+
+        return NextResponse.json({ messages }, { status: 200 });
+    } catch (error) {
+        console.error('Database Error:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
+
